9c345e219ab2ea8e846ef52949c37508a6978936
Rianne Schouwstra <rianne.schouwstra@hva.nl>
2016-02-03 15:12:33 +0100

    toggle works

--- opdracht5/static/app.js
+++ opdracht5/static/app.js
@@ -3,18 +3,31 @@
 
 	var app = {
 		init: function(){
-
+			routes.init();
 		}
-	}
+	};
+
 	var routes = {
 		init: function(){
-
+			window.addEventListener('hashchange', function(hashObj) {
+				var hash = hashObj.newURL.split('#')[1];
+				sections.toggle(hash);
+			}, false); // prevents default function (off)
 		}
-	}
+	};
 
 	var sections = {
 		toggle: function(route){
-			
+			if (route === 'start'){
+				document.getElementById('start').style.display='block';
+				document.getElementById('best').style.display='none';
+			} else {
+				document.getElementById('start').style.display='none';
+				document.getElementById('best').style.display='block';
+			}
 		}
-	}
+	};
+
+	app.init();
+
 })();
